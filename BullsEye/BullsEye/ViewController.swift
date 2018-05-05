7e741c5468814ec73475eb926615202e6543d5e0
Owen Henley <owenhenley@me.com>
2018-05-04 22:57:26 -0600

    Slider number randomiser
    Continued from the RW email course. Pt. 5

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -6,13 +6,20 @@
 //  Copyright © 2018 Owen Henley. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider:UISlider!
+    
     var currentValue:Int = 0
-
+    var targetValue: Int = 0
+	
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,7 +30,7 @@ class ViewController: UIViewController {
     
     // Hit Me Button
     @IBAction func showAlert() {
-		let message = "The value of the slider is:\(currentValue)"
+        let message = "The value of the slider is:\(currentValue)" + "\nThe target value is: \(targetValue)"
 		
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         
