e31ea8da2ee24a7ee2cd1e2450a8d8973875ddba
Owen Henley <owenhenley@me.com>
2018-05-05 15:05:44 -0600

    SinglePhoto App
    Super basic image viewer app from Apple's "Into To App Development" eBook

---
+++ SinglePhoto/SinglePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Owen Henley on 5/5/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
