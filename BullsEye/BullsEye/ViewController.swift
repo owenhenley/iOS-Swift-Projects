5d56ef50aeffd85319eee27c53ca865ddc087926
Owen Henley <owenhenley@me.com>
2018-05-07 23:43:04 -0600

    Adding functions

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,21 +11,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var slider:UISlider!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     var currentValue:Int = 0
     var targetValue: Int = 0
-	
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        targetValue = 1 + Int(arc4random_uniform(100))
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue  = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
     
     // Hit Me Button
@@ -39,6 +49,10 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        
+        updateLabels()
     }
 	
     @IBAction func sliderMoved(_ slider:UISlider) {
