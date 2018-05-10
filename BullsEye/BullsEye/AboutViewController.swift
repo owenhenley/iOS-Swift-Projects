44868a4127f6f2cc616f4edbc36396717060653e
Owen Henley <owenhenley@me.com>
2018-05-10 14:57:32 -0600

    More connections
    Added scorecards and resets
    
    info viewController

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Owen Henley on 5/10/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -6,57 +6,94 @@
 //  Copyright © 2018 Owen Henley. All rights reserved.
 //
 
+// target value
+
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var score = 0
+    var round = 0
+    var currentValue = 0
+    var targetValue = 0
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue:Int = 0
-    var targetValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startNewRound()
+        startNewGame()
         updateLabels()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func startNewRound() {
-        targetValue = 1 + Int(arc4random_uniform(100))
-        currentValue  = 50
-        slider.value = Float(currentValue)
-    }
-    
-    func updateLabels() {
-        targetLabel.text = String(targetValue)
-    }
-    
-    // Hit Me Button
     @IBAction func showAlert() {
-        let message = "The value of the slider is:\(currentValue)" + "\nThe target value is: \(targetValue)"
-		
-        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        let difference = abs(currentValue - targetValue) // abs - If whatever is inside paranthases is a negative, will be made positive
+        var points = 100 - difference
         
-        let action = UIAlertAction (title: "OK", style: .default, handler: nil)
+        let title: String
+        if difference == 0 {
+            title = "Perfect!" + "\n100 Bonus Points!"
+            points += 100
+        } else if difference < 5 {
+            title = "So close!" + "\n20 Bonus Points!"
+            points += 20
+        } else if difference < 10 {
+            title = "Close, but no cigar..."
+        } else {
+            title = "Not even close...🤦‍♂️"
+            points = 0
+        }
+        score += points // Score = score + points
+        
+        let message = "You scored \(points) points!"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: { action in
+                                self.startNewRound()
+                                self.updateLabels()
+                            })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
-        
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
         updateLabels()
     }
-	
-    @IBAction func sliderMoved(_ slider:UISlider) {
-        currentValue = lroundf(slider.value)
+    
+    func startNewRound() {
+        round += 1 // increments the round 1 by 1 every time its called
+        targetValue = 1 + Int(arc4random_uniform(100)) // randomises the target value
+        currentValue = 50 // resets the slider to the centre
+        slider.value = Float(currentValue)  // sets the value to a float
+    }
+    
+    func startNewGame() {
+        score = 0 // resets score and round
+        round = 0
+        startNewRound() //calls for a new round from the above function
+    }
+    
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
