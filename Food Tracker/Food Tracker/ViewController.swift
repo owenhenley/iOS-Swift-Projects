649a71de36671cd9ff9664a2642e5fd6280e22ab
Owen Henley <owenhenley@me.com>
2018-06-23 14:02:59 -0600

    Started Food Tracker

---
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Owen Henley on 6/23/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+}
+
