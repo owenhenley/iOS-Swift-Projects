6a137894dd6b3aaa5c98c431833ee5570e1bca66
Owen Henley <owenhenley@me.com>
2018-08-19 21:37:37 -0600

    Added MapKit App

---
+++ HonoluluArtMapKit/HonoluluArt/Artwork.swift
@@ -0,0 +1,76 @@
+//
+//  Artwork.swift
+//  HonoluluArt
+//
+//  Created by Owen Henley on 8/19/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import Contacts
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+    
+    // MARK: - Properties
+    // markerTintColor for disciplines: Sculpture, Plaque, Mural, Monument, other
+    var markerTintColor: UIColor  {
+        switch discipline {
+        case "Monument":
+            return .red
+        case "Mural":
+            return .cyan
+        case "Plaque":
+            return .blue
+        case "Sculpture":
+            return .purple
+        default:
+            return .green
+        }
+    }
+    
+    var imageName: String? {
+        if discipline == "Sculpture" { return "Statue" }
+        return "Flag"
+    }
+    
+    // Annotation right callout accessory opens this mapItem in Maps app
+    func mapItem() -> MKMapItem {
+        let addressDict = [CNPostalAddressStreetKey: subtitle!]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = title
+        return mapItem
+    }
+    
+    init?(json: [Any]) {
+        // 1
+        self.title = json[16] as? String ?? "No Title"
+        self.locationName = json[11] as! String
+        self.discipline = json[15] as! String
+        // 2
+        if let latitude = Double(json[18] as! String),
+            let longitude = Double(json[19] as! String) {
+            self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        } else {
+            self.coordinate = CLLocationCoordinate2D()
+        }
+    }
+}
